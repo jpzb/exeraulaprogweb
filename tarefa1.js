1303f201513f6b6342d175cb26fc9232a41bbdef
João Pedro Zamborlini Barcellos <91471627+jpzb@users.noreply.github.com>
2021-10-18 12:58:21 -0300

    Add files via upload

---
+++ tarefa1.js
@@ -0,0 +1,25 @@
+const subtitulos = document.querySelectorAll('h2');
+const topicos = new Array();
+const ids = new Array();
+subtitulos.forEach(subtitulo => {
+    topicos.push(subtitulo.textContent);
+    ids.push(subtitulo.id);
+    const ancora = document.createElement('a');
+    ancora.setAttribute('name', subtitulo.textContent);
+    subtitulo.append(ancora);
+    const retorno = document.createElement('a');
+    retorno.setAttribute('href', '#');
+    retorno.textContent = "Subir";
+    subtitulo.parentElement.insertBefore(retorno, subtitulo.nextElementSibling);
+});
+const lista = document.querySelector('ol');
+topicos.forEach(topico => {
+    const item = document.createElement('li');
+    lista.append(item);
+    const link = document.createElement('a');
+    link.setAttribute('href', `#${topico}`);
+    link.textContent = topico; 
+    item.append(link);
+});
+
+// INCOMPLETO
